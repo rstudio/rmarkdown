719b8155e9c959d2b664ffb28ec2c8ebb63e0474
JJ Allaire <jj@rstudio.org>
2014-04-01 05:09:27 -0400

    more work on dependency helper functions

--- R/html_dependencies.R
+++ R/html_dependencies.R
@@ -1,24 +1,39 @@
 
-# collect all dependencies provided by the passed knit_meta (also merge in
-# any dependencies implied by our document theme)
-collect_html_dependencies <- function(theme, knit_meta) {
 
-  # list of dependencies to return (start with jquery and bootstrap if
-  # have a document theme defined)
+
+# resolve dependencies for html_document
+resolve_html_document_dependencies <- function(theme, knit_meta) {
+
   if (!is.null(theme))
-    dependencies <- list(jquery_dependency(), bootstrap_dependency(theme))
+    format_deps <- list(jquery_dependency(), bootstrap_dependency(theme))
+  else
+    format_deps <- NULL
+
+  resolve_html_dependencies(format_deps, knit_meta)
+}
+
+
+# resolve html dependencies (inclusive of a format's built-in dependencies)
+resolve_html_dependencies <- function(format_deps, knit_meta) {
+
+  # list of dependencies to return (start with format_deps)
+  if (!is.null(format_deps))
+    dependencies <- format_deps
   else
     dependencies <- list()
-
 
   # knit_meta is a list of 'meta' attributes returned from custom knit_print
   # functions. since the 'meta' attribute could either be an html dependency or
   # a list of dependencies we recurse on lists that aren't named
   for (dep in knit_meta) {
-    if (is.null(names(dep)))
-      dependencies <- append(dependencies, collect_html_dependencies(theme, dep))
-    else
-      dependencies[[length(dependencies) + 1]] <- html_dependency(dep)
+    if (is.null(names(dep))) {
+      inner_dependencies <- collect_html_dependencies(theme, dep)
+      dependencies <- append(dependencies, inner_dependencies)
+    }
+    else if (is_html_dependency(dep)) {
+      validate_html_dependency(dep)
+      dependencies[[length(dependencies) + 1]] <- dep
+    }
   }
 
   #
@@ -30,60 +45,75 @@ collect_html_dependencies <- function(theme, knit_meta) {
   unique(dependencies)
 }
 
-# create an html dependency based on a list (typically acquired from knit_meta,
-# but could also be one of the built in dependencies above).
-html_dependency <- function(dep_list) {
+# create and validate an html_dependency from the passed arguments
+html_dependency <- function(name,
+                            version,
+                            path,
+                            script = NULL,
+                            stylesheet = NULL,
+                            head = NULL) {
 
-  # validate required fields
-  if (is.null(dep_list$name))
-    stop("name for html dependency not provided", call. = FALSE)
-  if (is.null(dep_list$version))
-    stop("version for html dependency not provided", call. = FALSE)
-  if (is.null(dep_list$path))
-    stop("path for html dependency not provided", call. = FALSE)
-  if (!file.exists(dep_list$path))
-    stop("path for html dependency not found: ", dep_list$path, call. = FALSE)
+  dep <- structure(list(name = name,
+                        version = version,
+                        path = path,
+                        script = script,
+                        stylesheet = stylesheet,
+                        head = head),
+                   class = "html_dependency")
 
-  # validate that other fields are known
-  fields <- names(dep_list)
-  invalid_fields <- fields[! fields %in%
-                             c("name", "version", "path", "script", "stylesheet", "head")]
-  if (length(invalid_fields) > 0) {
-    stop("unrecoginzed fields specified in html dependency: ",
-         paste(invalid_fields, sep = ", "), call. = FALSE)
-  }
-
-  # return the dependency info
-  structure(dep_list, class = "html_dependency")
+  validate_html_dependency(dep)
 }
-
-
 
 # create an html dependency for our embedded jquery
 jquery_dependency <- function()  {
-
-  name = "jquery"
-  version <- "1.11.0"
-  path <- rmarkdown_system_file(paste("rmd/h/", name, "-", version, sep=""))
-
-  html_dependency(list(name = name,
-                       version = version,
-                       path = path,
-                       script = "jquery.min.js"))
+  html_dependency(name = "jquery",
+                  version = "1.11.0",
+                  path = rmarkdown_system_file("rmd/h/jquery-1.11.0"),
+                  script = "jquery.min.js")
 }
 
 # create an html dependency for our embedded bootstrap
 bootstrap_dependency <- function(theme) {
-
-  name <- "bootstrap"
-  version <- "2.3.2"
-  path <- rmarkdown_system_file(paste("rmd/h/", name, "-", version, sep=""))
-  theme_css <- paste("css/", theme, ".min.css", sep="")
-
-  html_dependency(list(name = name,
-                       version = version,
-                       path = path,
-                       script = "js/bootstrap.min.js",
-                       stylesheet = c(theme_css, "css/bootstrap-responsive.min.css")))
+  html_dependency(name = "bootstrap",
+                  version = "2.3.2",
+                  path = rmarkdown_system_file("rmd/h/bootstrap-2.3.2"),
+                  script = "js/bootstrap.min.js",
+                  stylesheet = c(paste("css/", theme, ".min.css", sep=""),
+                                 "css/bootstrap-responsive.min.css"))
 }
+
+is_html_dependency <- function(list) {
+  inherits(list, "html_dependency")
+}
+
+# validate that the passed list is a correctly formed html_dependency
+validate_html_dependency <- function(list) {
+
+  # ensure it's the right class
+  if (!is_html_dependency(list))
+    stop("passed object is not of class html_dependency", call. = FALSE)
+
+  # validate required fields
+  if (is.null(list$name))
+    stop("name for html_dependency not provided", call. = FALSE)
+  if (is.null(list$version))
+    stop("version for html_dependency not provided", call. = FALSE)
+  if (is.null(list$path))
+    stop("path for html_dependency not provided", call. = FALSE)
+  if (!file.exists(list$path))
+    stop("path for html_dependency not found: ", list$path, call. = FALSE)
+
+  # validate that other fields are known
+  fields <- names(list)
+  invalid_fields <- fields[! fields %in%
+                             c("name", "version", "path", "script", "stylesheet", "head")]
+  if (length(invalid_fields) > 0) {
+    stop("unrecoginzed fields specified in html_dependency: ",
+         paste(invalid_fields, sep = ", "), call. = FALSE)
+  }
+
+  list
+}
+
+
 
