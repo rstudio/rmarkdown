8b58b2af990f23fff57adbecbd70c67f4c9fb5b2
Jonathan McPherson <jonathan@rstudio.com>
2014-04-09 11:11:57 -0700

    remove Shiny reactive adapters (moving to Shiny)

--- R/shiny.R
+++ R/shiny.R
@@ -1,33 +1,35 @@
 #' Run a Shiny document
 #'
-#' Render a Shiny document and start Shiny to show the output.
+#' Start a Shiny server for the given document, and render it for display.
 #'
-#' @param input Input file
-#' @param shiny_args Arguments to the \pkg{shiny} \code{\link{runApp}} function.
+#' @param file Input file
+#' @param auto_reload If \code{TRUE} (the default), automatically reload the
+#'   Shiny application when the input file is changed.
+#' @param shiny_args Arguments to the \code{\link{runApp}} function.
 #' @param ... Additional arguments to \code{\link{render}}.
 #'
 #' @return Invisible NULL.
 #'
-#' @details The \code{run_document} function fully renders an R Markdown
-#'   document to HTML (using \code{\link{render}}), creates a Shiny application
-#'   from the result, and then invokes \code{runApp} to view the application.
+#' @details The \code{run_document} function runs a Shiny document by starting a
+#'   \pkg{shiny} server associated with the document, and rendering the document
+#'   as necessary when connections to the server are made.
 #'
-#'   Note that unlike \code{render}, \code{run_document} does not produce a
-#'   file on disk that can be saved or shared.
+#' @note Unlike \code{\link{render}}, \code{run_document} does not render the
+#'   document to a file on disk. To view the document, point a Web browser to
+#'   the URL displayed when the server starts. In most cases a Web browser will
+#'   be started automatically to view the document; see \code{launch.browser} in
+#'   the \code{\link{runApp}} documentation for details.
 #'
 #' @examples
 #' \dontrun{
 #'
-#' # Render shiny_doc.Rmd and start a server for the result on port 8241.
+#' # Run the Shiny document "shiny_doc.Rmd" on port 8241
+#'
 #' rmarkdown::run_document("shiny_doc.Rmd", shiny_args = list(port = 8241))
 #'
 #' }
 #' @export
 run_document <- function(file, auto_reload = TRUE, shiny_args = NULL, ...) {
-
-  shinyPlot <- create_adapter(renderPlot, plotOutput)
-  shinyTable <- create_adapter(renderTable, tableOutput)
-  shinyPrint <- create_adapter(renderPrint, verbatimTextOutput)
 
   server <- function(input, output, session) {
     reactive_file <- if (auto_reload)
@@ -37,7 +39,8 @@ run_document <- function(file, auto_reload = TRUE, shiny_args = NULL, ...) {
     doc <- reactive({
       output_dest <- tempfile()
       on.exit(unlink(output_dest), add = TRUE)
-      output_dest <- render(reactive_file(), output_file = output_dest, ...)
+      output_dest <- render(reactive_file(), output_file = output_dest,
+                            runtime = "shiny", ...)
       paste(readLines(output_dest), collapse="\n")
     })
     output$`__reactivedoc__` <- renderUI({
@@ -48,16 +51,5 @@ run_document <- function(file, auto_reload = TRUE, shiny_args = NULL, ...) {
                  server = server)),
             shiny_args)
   do.call(shiny::runApp, args)
-}
-
-create_adapter <- function(renderFunc, outputFunc) {
-  function(expr, env = parent.frame(), quoted=FALSE) {
-    installExprFunction(expr, "func", env, quoted)
-
-    id <- shiny:::createUniqueId(8)
-    o <- get("output", pos = env)
-    o[[id]] <- renderFunc(func())
-    outputFunc(id)
-  }
 }
 
