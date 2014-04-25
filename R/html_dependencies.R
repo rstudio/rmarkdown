7f5a854d88a560690ea931b894c654e2a11b4850
JJ Allaire <jj@rstudio.org>
2014-04-24 20:53:28 -0400

    don't merge meta and head dependencies

--- R/html_dependencies.R
+++ R/html_dependencies.R
@@ -121,15 +121,11 @@ html_dependencies_for_document <- function(knit_meta, format_deps = NULL) {
         dependencies[[dep$name]]$path <- dep$path
         dependencies[[dep$name]]$script <- dep$script
         dependencies[[dep$name]]$stylesheet <- dep$stylesheet
+        dependencies[[dep$name]]$meta <- dep$meta
+        dependencies[[dep$name]]$head <- dep$head
         dependencies[[dep$name]]$external <- dep$external
       }
-
-      # consolidate other fields
-      dependencies[[dep$name]]$meta <-
-        merge_lists(dependencies[[dep$name]]$meta, dep$meta)
-      dependencies[[dep$name]]$head <-
-        unique(c(dependencies[[dep$name]]$head, dep$head))
-      # first instance of this library, just copy over all the fields
+    # first instance of this library, just copy over all the fields
     } else {
       dependencies[[dep$name]] <- dep
     }
