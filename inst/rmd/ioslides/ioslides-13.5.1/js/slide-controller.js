9083d5daf7e1de008396735d6e6416d3e3409e18
Jonathan McPherson <jonathan@rstudio.com>
2014-04-29 17:07:25 -0700

    ioslides: ignore postMessage from unrecognized origin

--- inst/rmd/ioslides/ioslides-13.5.1/js/slide-controller.js
+++ inst/rmd/ioslides/ioslides-13.5.1/js/slide-controller.js
@@ -85,7 +85,7 @@ SlideController.prototype.onMessage_ = function(e) {
   // from file:// though.
   // TODO: It would be dope if FF implemented location.origin!
   if (e.origin != ORIGIN_ && ORIGIN_.indexOf('file://') != 0) {
-    alert('Someone tried to postMessage from an unknown origin');
+    // Ignore messages from unrecognized origins
     return;
   }
 
