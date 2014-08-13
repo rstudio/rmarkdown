4af48f1ce798d84885b9a077bfac4670433affe2
JJ Allaire <jj@rstudio.org>
2014-08-13 09:48:55 -0400

    add css option for slidy

--- R/slidy_presentation.R
+++ R/slidy_presentation.R
@@ -38,6 +38,7 @@ slidy_presentation <- function(incremental = FALSE,
                                highlight = "default",
                                mathjax = "default",
                                template = "default",
+                               css = NULL,
                                includes = NULL,
                                keep_md = FALSE,
                                lib_dir = NULL,
@@ -62,6 +63,10 @@ slidy_presentation <- function(incremental = FALSE,
   # content includes
   args <- c(args, includes_to_pandoc_args(includes))
 
+  # additional css
+  for (css_file in css)
+    args <- c(args, "--css", pandoc_path_arg(css_file))
+  
   # pre-processor for arguments that may depend on the name of the
   # the input file (e.g. ones that need to copy supporting files)
   pre_processor <- function(metadata, input_file, runtime, knit_meta, files_dir,
