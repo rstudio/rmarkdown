7ea120b6de332c60a7b1f7a41cd9c8cc74cb5f44
hadley <h.wickham@gmail.com>
2014-08-26 16:14:08 -0500

    Initial version of html_vignette

---
+++ R/html_vignette.R
@@ -0,0 +1,32 @@
+#' Convert to an HTML vignette.
+#' 
+#' A HTML vignette is a lightweight alternative to \code{\link{html_document}}
+#' suitable for inclusion in packages to be released to CRAN. It reduces the
+#' size of a basic vignette from 100k to around 10k.
+#' 
+#' Compared to \code{html_document}, it:
+#' 
+#' \itemize{
+#'   \item never uses retina figures
+#'   \item has a smaller default figure size
+#'   \item uses a custom css stylesheet
+#'  }
+#'
+#' @inheritParams html_document
+#' @return R Markdown output format to pass to \code{\link{render}}
+#' @export
+html_vignette <- function(fig_width = 3.9,
+                          fig_height = 3.5,
+                          css = NULL,
+                          ...) {
+  
+  if (is.null(css)) {
+    css <- system.file("vignette.css", package = "rmarkdown")
+  }
+  
+  html_document(fig_width = fig_width, 
+                fig_height = fig_height, 
+                fig_retina = FALSE,
+                css = css, 
+                ...)
+}
