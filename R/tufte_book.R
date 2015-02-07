e633d4905c351f166619a46e2f3b1cd64ed00d9b
Amos Elberg <amos.elberg@gmail.com>
2015-02-06 22:35:11 -0500

    This is an adaptation of the Tufte-Book class for rmarkdown.  This is more flexible than the Tufte-Handbook class and has quite a few more features.  Two notes:  First, I don't know how to make this appear in the Rstudio templates chooser.  Second, there are a small number of incomplete bits.  I haven't gotten it to compile with xetex or luatex; subsubsections only work inconsistently; and the TOC numbers aren't right-justified.  I'm pull-requesting this now because its nevertheless useable, and to solicit attention to the problem of getting xetex and luatex working, which seem to be beyond my TeX skills.

---
+++ R/tufte_book.R
@@ -0,0 +1,86 @@
+
+#' Tufte book format (PDF)
+#' 
+#' Template for creating a book according to the style of 
+#' Edward R. Tufte and Richard Feynman. 
+#' 
+#' @inheritParams pdf_document
+#' 
+#' @export
+tufte_book <- function(fig_width = 4,
+                          fig_height = 2.5,
+                          fig_crop = TRUE,
+                          highlight = "default",
+                          keep_tex = FALSE,
+                          includes = NULL,
+                          pandoc_args = NULL,
+                          toc = TRUE,
+                          toc_depth = 3,
+                          latex_engine = "xelatex",
+                          number_sections = TRUE,
+                          ...
+                          ) {
+  
+  # resolve default highlight
+  if (identical(highlight, "default"))
+    highlight <- "pygments"
+  
+  # get the tufte handlout template
+  template <-  system.file(
+    "rmarkdown/templates/tufte_book/resources/tufte-book.tex", 
+    package = "rmarkdown"
+  )
+  
+  # call the base pdf_document format with the appropriate options
+  format <- rmarkdown::pdf_document(fig_width = fig_width,
+                                    fig_height = fig_height,
+                                    fig_crop = fig_crop,
+                                    highlight = highlight,
+                                    template = template,
+                                    keep_tex = keep_tex,
+                                    latex_engine = latex_engine,
+                                    includes = includes,
+                                    pandoc_args = pandoc_args,
+                                    toc = toc,
+                                    toc_depth = toc_depth,
+                                    number_sections = TRUE,
+                                    ...
+                                    )
+                        
+  
+  # create knitr options (ensure opts and hooks are non-null)
+  knitr_options <- knitr_options_pdf(fig_width, fig_height, fig_crop)
+  if (is.null(knitr_options$opts_knit))
+    knitr_options$opts_knit <- list()
+  if (is.null(knitr_options$knit_hooks))
+    knitr_options$knit_hooks <- list()
+  
+  # set options
+  knitr_options$opts_chunk$tidy <- TRUE
+  knitr_options$opts_knit$width <- 45
+  
+  # set hooks for special plot output
+  knitr_options$knit_hooks$plot <- function(x, options) {
+    
+    # determine caption (if any)
+    caption <- ifelse(is.null(options$fig.cap), 
+                      "",
+                      paste("\\caption{", options$fig.cap, "}\n", sep = ""))
+    
+    # determine figure type
+    if (isTRUE(options$fig.margin)) 
+      figtype <- "marginfigure"
+    else if (isTRUE(options$fig.fullwidth))
+      figtype <- "figure*"
+    else
+      figtype <- "figure"
+    
+    # return the latex
+    paste(sprintf('\\begin{%s}\n \\includegraphics{%s}\n%s\\end{%s}\n',
+                  figtype, x, caption, figtype))
+  }
+  
+  # override the knitr settings of the base format and return the format
+  format$knitr <- knitr_options
+  format
+}
