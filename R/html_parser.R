4848094871987d640c28f0bfae13b81b0aa8c137
Jonathan McPherson <jonathan@rstudio.com>
2015-02-16 15:35:38 -0800

    improve tests & documentation

--- R/html_parser.R
+++ R/html_parser.R
@@ -1,4 +1,11 @@
+# This file implements a simple HTML parser designed to locate tags and
+# attribute values in HTML emitted by pandoc. Herein, we process input HTML one 
+# character at a time and use a simple state machine to determine where to go 
+# go next.
 
+# Parse states--each is a list of rules which govern transitions to other
+# states. Any character that doesn't match one of the rules causes the parser
+# to state in the state and store the character as content.
 .H_TEXT <- list(
   "<" = ".H_NODE")
 
@@ -29,6 +36,7 @@
 .H_ATTR_VALUE <- list(
   "'" = ".H_SINGLE_ATTR",
   "\"" = ".H_DOUBLE_ATTR",
+  "white" = ".H_ATTR_VALUE",
   "any" = ".H_UNQUOTED_ATTR")
 
 .H_SINGLE_ATTR <- list(
@@ -41,7 +49,15 @@
   "white" = ".H_ATTR_LIST",
   ">" = ".H_TEXT")
 
-html_extract_values <- function(html, callback) {
+# Extract HTML attribute values, invoking a callback on each. The callback 
+# receives the tag, attribute name, and attribute value.
+# For instance, extracting values from:
+#    <img class="bordered" src="foo.png">
+# results in two callbacks:
+#    callback("img", "class", "bordered")
+#    callback("img", "src", "foo.png")
+html_extract_values <- function(html, callback = NULL, show_parse = FALSE) {
+  # state information (rewritten as we go)
   i <- 1
   end <- nchar(html)
   state <- .H_TEXT
@@ -53,14 +69,22 @@ html_extract_values <- function(html, callback) {
   
   # function to transition to a new state
   do_transition <- function(new_state) {
+    if (old_state == new_state) {
+      return(state)
+    }
     if (old_state == ".H_TAG")
       cur_tag <<- contents
     else if (old_state == ".H_ATTR")
       cur_attr <<- contents
-    else if (old_state == ".H_DOUBLE_ATTR" || 
-        old_state == ".H_SINGLE_ATTR" ||
-        old_state == ".H_UNQUOTED_ATTR") {
+    else if (!is.null(callback) && 
+        (old_state == ".H_DOUBLE_ATTR" || 
+         old_state == ".H_SINGLE_ATTR" ||
+         old_state == ".H_UNQUOTED_ATTR")) {
       callback(cur_tag, cur_attr, contents)    
+    }
+    if (show_parse) {
+      print(contents)
+      print(new_state)
     }
     contents <<- ""
     transition <<- TRUE
@@ -90,12 +114,13 @@ html_extract_values <- function(html, callback) {
         }
       } else if (identical(pat, "white") && 
                  (ch == " "  || ch == "\n" || ch == "\t" || ch == "\v")) {
-        # whitespace character
+        # whitespace character--consume it
         state <- do_transition(state[[rule_idx]])
         break
       } else if (identical(pat, "any")) {
         # any character
         state <- do_transition(state[[rule_idx]])
+        break
       } else if (identical(pat, ch)) {
         # specific character--consume it
         state <- do_transition(state[[rule_idx]])
