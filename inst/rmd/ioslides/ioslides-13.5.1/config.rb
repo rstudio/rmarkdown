86d7a591a31f98301ab7008d760947dbb74bf791
Jameson Quinn <jameson.quinn+github@gmail.com>
2015-03-19 12:12:59 -0400

    Fix config.rb for more-similar output

--- inst/rmd/ioslides/ioslides-13.5.1/config.rb
+++ inst/rmd/ioslides/ioslides-13.5.1/config.rb
@@ -8,7 +8,7 @@ images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed #:expanded or :nested or :compact or :compressed
+output_style = :expanded #:expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
