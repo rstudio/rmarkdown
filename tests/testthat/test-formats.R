b900adaac7559fd798ab07a3afa3814dd342c684
JJ Allaire <jj@rstudio.org>
2015-03-28 15:23:34 -0400

    add test for basic function of each format

---
+++ tests/testthat/test-formats.R
@@ -0,0 +1,24 @@
+context("formats")
+
+test_that("formats successfully produce a document", {
+  
+  testFormat <- function(output_format) {
+    output_file <- tempfile(fileext = ".html")
+    capture.output(render("test-formats.Rmd", 
+                          output_format = output_format,
+                          output_file = output_file))
+    expect_true(file.exists(output_file))
+  }
+  
+  testFormat(html_document())
+  testFormat(html_fragment())
+  testFormat(html_vignette())
+  testFormat(ioslides_presentation())
+  testFormat(slidy_presentation())
+  testFormat(md_document())
+  testFormat(pdf_document())
+  testFormat(beamer_presentation())
+  testFormat(tufte_handout())
+  testFormat(word_document())
+
+})
