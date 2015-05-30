3d46213d750fd4ebb83e73d0df357f081c73f49f
JJ Allaire <jj@rstudio.org>
2015-05-30 14:48:21 -0400

    Use built-in templates for LaTeX and Beamer if pandoc >= 1.14 (Fixes #439)
    Pandoc is now generating new LaTeX and defining new variables that aren't supported by our older template.

--- R/beamer_presentation.R
+++ R/beamer_presentation.R
@@ -79,8 +79,13 @@ beamer_presentation <- function(toc = FALSE,
 
   # template path and assets
   if (identical(template, "default"))
-    args <- c(args, "--template",
-              pandoc_path_arg(rmarkdown_system_file("rmd/beamer/default.tex")))
+    # beginning with pandoc 1.14 the "default" template is the one built in
+    # with pandoc (this is because pandoc emits some new LaTeX and variables
+    # starting with v1.14 that our current template doesn't suport)
+    if (!pandoc_available("1.14")) {
+      args <- c(args, "--template",
+                pandoc_path_arg(rmarkdown_system_file("rmd/beamer/default.tex")))
+    }
   else if (!is.null(template))
     args <- c(args, "--template", pandoc_path_arg(template))
 
--- R/pdf_document.R
+++ R/pdf_document.R
@@ -102,8 +102,13 @@ pdf_document <- function(toc = FALSE,
 
   # template path and assets
   if (identical(template, "default"))
-    args <- c(args, "--template",
-              pandoc_path_arg(rmarkdown_system_file("rmd/latex/default.tex")))
+    # beginning with pandoc 1.14 the "default" template is the one built in
+    # with pandoc (this is because pandoc emits some new LaTeX and variables
+    # starting with v1.14 that our current template doesn't suport)
+    if (!pandoc_available("1.14")) {
+      args <- c(args, "--template",
+                pandoc_path_arg(rmarkdown_system_file("rmd/latex/default.tex")))
+    }
   else if (!is.null(template))
     args <- c(args, "--template", pandoc_path_arg(template))
 
