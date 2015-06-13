e5ae394baac8a1469d78932b1561c909b2f50907
JJ Allaire <jj@rstudio.org>
2015-06-13 06:19:08 -0400

    more comments on theme workaround

--- R/html_dependencies.R
+++ R/html_dependencies.R
@@ -13,8 +13,14 @@ html_dependency_jquery <- function()  {
 html_dependency_bootstrap <- function(theme) {
   
   # inline bootstrap theme b/c pandoc 1.14 base64 encoding 
-  # somehow borks up bootstrap.min.css 
+  # somehow borks up reading bootstrap.min.css. it also borks
+  # up the handling of embedded fonts (as bootstrap themes that
+  # reference embedded fonts actually end up with errors). note
+  # that is implies that themes won't get their embedded fonts
+  # but this is superior to having rendering fail with an error!
+  #
   # see pandoc bug filed here: https://github.com/jgm/pandoc/issues/2224
+  
   theme_css <- rmarkdown_system_file(paste0("rmd/h/bootstrap-3.3.1/css/", 
                                             theme, ".min.css"))
   theme_style_tag <- paste(c('<style type="text/css">',
