47417fe5c9e13b57a559d33e456f0733998fc56c
JJ Allaire <jj@rstudio.org>
2016-03-29 14:17:49 -0400

    enhance test coverage

--- tests/testthat/test-site.R
+++ tests/testthat/test-site.R
@@ -1,23 +1,35 @@
 context("site")
 
-test_that("render_site renders all pages in a directory", {
+test_that("render_site", {
 
   skip_on_cran()
 
   # copy our demo site to a tempdir
   site_dir <- tempfile()
   dir.create(site_dir)
-  file.copy(c("site/_site.yml", "site/index.md", "site/PageA.Rmd",
-              "site/PageB.Rmd", "site/PageC.md", "site/styles.css"),
-            site_dir, recursive = TRUE)
+  files <- c("_site.yml", "index.md", "PageA.Rmd",
+             "PageB.Rmd", "PageC.md", "styles.css",
+             "script.R", "docs.txt")
+  file.copy(file.path("site", files), site_dir, recursive = TRUE)
 
   # render it
   capture.output(render_site(site_dir))
 
-  # did the html files get rendered?
-  expect_true(file.exists(file.path(site_dir, "_site", "index.html")))
-  expect_true(file.exists(file.path(site_dir, "_site", "PageA.html")))
-  expect_true(file.exists(file.path(site_dir, "_site", "PageB.html")))
-  expect_true(file.exists(file.path(site_dir, "_site", "PageC.html")))
-  expect_true(file.exists(file.path(site_dir, "_site", "styles.css")))
+  # did the html files get rendered and the css get copied?
+  html_files <- c("index.html", "PageA.html", "PageB.html", "PageC.html")
+  html_files <- file.path(site_dir, "_site", html_files)
+  expect_true(all(file.exists(html_files)))
+
+  # moved directories
+  moved <- c("lib", "PageA_files")
+  expect_true(all(!file.exists(file.path(site_dir, moved))))
+  expect_true(all(file.exists(file.path(site_dir, "_site", moved))))
+
+  # respected includes
+  included <- "script.R"
+  expect_true(all(file.exists(file.path(site_dir, "_site", included))))
+
+  # respected excluded
+  excluded <- "docs.txt"
+  expect_true(all(!file.exists(file.path(site_dir, "_site", excluded))))
 })
