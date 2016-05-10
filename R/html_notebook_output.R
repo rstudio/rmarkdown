df5d3efee86f22dda3d2cd0880b8473f376d697e
Kevin Ushey <kevinushey@gmail.com>
2016-05-10 14:54:45 -0500

    add 'iframe' argument

--- R/html_notebook_output.R
+++ R/html_notebook_output.R
@@ -9,6 +9,7 @@
 #'   will be obtained by reading the file.
 #' @param bytes The bytewise representation of content.
 #' @param html Arbitrary HTML content to insert.
+#' @param iframe Boolean; should the generated HTML be shown in an iframe?
 #'
 #' @name html_notebook_output
 NULL
@@ -25,8 +26,9 @@ html_notebook_render_base64_data <- function(path = NULL,
 
 #' @name html_notebook_output
 #' @export
-html_notebook_output_html <- function(html) {
-  html_notebook_annotated_output(html, "html")
+html_notebook_output_html <- function(html, iframe = TRUE) {
+  meta <- list(iframe = isTRUE(iframe))
+  html_notebook_annotated_output(html, "html", meta)
 }
 
 #' @name html_notebook_output
