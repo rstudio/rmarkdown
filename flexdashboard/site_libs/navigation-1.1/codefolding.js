f545a24e866361430ab6a9f41d5eeb41a333ee18
JJ Allaire <jj@rstudio.org>
2016-05-18 10:51:06 -0400

    update navigation library

--- flexdashboard/site_libs/navigation-1.1/codefolding.js
+++ flexdashboard/site_libs/navigation-1.1/codefolding.js
@@ -1,5 +1,4 @@
 
-
 window.initializeCodeFolding = function(show) {
 
   // handlers for show-all and hide all
@@ -31,7 +30,7 @@ window.initializeCodeFolding = function(show) {
     $(this).detach().appendTo(div);
 
     // add a show code button right above
-    var showCodeText = $('<span class="text-muted">' + (show ? 'Hide' : 'Code') + '</span>');
+    var showCodeText = $('<span>' + (show ? 'Hide' : 'Code') + '</span>');
     var showCodeButton = $('<button type="button" class="btn btn-default btn-xs code-folding-btn pull-right"></button>');
     showCodeButton.append(showCodeText);
     showCodeButton
