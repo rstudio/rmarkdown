24c6caa754ebedfd140f7869e01073cc18e30303
aoles <andrzej.oles@gmail.com>
2016-06-10 11:58:47 +0200

    byte-encode rmd source to retain utf-8 encoding

--- inst/rmd/h/navigation-1.1/sourceembed.js
+++ inst/rmd/h/navigation-1.1/sourceembed.js
@@ -3,7 +3,13 @@
 window.initializeSourceEmbed = function(filename) {
   $("#rmd-download-source").click(function() {
     var src = window.atob($("#rmd-source-code").html());
-    var blob = new Blob([src], {type: "text/x-r-markdown"});
+
+    var uint8 = new Uint8Array(src.length);
+    for (var i = 0; i < uint8.length; i++) {
+      uint8[i] = src.charCodeAt(i);
+    }
+
+    var blob = new Blob([uint8], {type: "text/x-r-markdown"});
     saveAs(blob, filename);
   });
 };
