ba5c181afbb0622939fe0930a3b59a6ac8d279f6
JJ Allaire <jj@rstudio.org>
2016-07-12 05:59:23 -0400

    update prune-mathjax script

--- tools/prune-mathjax.sh
+++ tools/prune-mathjax.sh
@@ -21,9 +21,15 @@ rm -f $MATHJAX_DIR/.gitignore
 rm -f $MATHJAX_DIR/bower.json
 rm -f $MATHJAX_DIR/README.md
 rm -f $MATHJAX_DIR/README-branch.txt
+rm -f $MATHJAX_DIR/CONTRIBUTING.md
+rm -f $MATHJAX_DIR/.travis.yml
+rm -f $MATHJAX_DIR/composer.json
+rm -f $MATHJAX_DIR/package.json
+
 rm -rf $MATHJAX_DIR/docs
 rm -rf $MATHJAX_DIR/test
 rm -rf $MATHJAX_DIR/unpacked
+
 
 rm -f $MATHJAX_DIR/config/Accessible-full.js
 rm -f $MATHJAX_DIR/config/Accessible.js
@@ -64,9 +70,9 @@ rm -rf $MATHJAX_DIR/jax/output/HTML-CSS/fonts/STIX-Web
 # renderer as follows: If Qt 4.8.x is rendering the page and the font requested
 # is a variant, always treat the font as successfully loaded even if its
 # character metrics don't match those expected.
-# 
+#
 # Without this change, MathJax will wait a considerable time for the font to
-# load before timing out and rendering math without the font. 
+# load before timing out and rendering math without the font.
 
  sed -i -e "s/this.div.style.fontSize=r;return(q===s)/this.div.style.fontSize=r;if(window.navigator.userAgent.indexOf(' Qt\/4.8.')>0\&\&(m.weight||m.style)){return true}else{return(q===s)}/" $MATHJAX_DIR/jax/output/HTML-CSS/jax.js
 
