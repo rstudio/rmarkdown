ef0d8a68409161dd2d349b3ea329e4c60a7330bb
Javier Luraschi <javierluraschi@hotmail.com>
2016-08-04 00:37:10 -0700

    add support for paging across columns

--- inst/rmd/h/pagedtable-0.0.1/js/pagedtable.js
+++ inst/rmd/h/pagedtable-0.0.1/js/pagedtable.js
@@ -1,32 +1,101 @@
 var PagedTable = function (pagedTable) {
-  var pageSize = 10;
-  var pageNumber = 0;
-  var pageCount;
-  var data;
-  var columnsData;
-
-  var columnsFromSource = function(json) {
-    if (json === null || json.length === 0)
-      return [];
-
-    var columns = json.columns;
-    if (columns.length > 10) {
-      columns = columns.slice(1, 10);
-      columns[10] = {
-        name: "...",
-        type: null
-      };
-    }
-
-    var typedColumns = columns.map(function(column) {
-      return {
-        name: column.name,
-        type: column.type,
-        align: column.align
-      };
+  var source = function(pagedTable) {
+    var sourceElems = [].slice.call(pagedTable.children).filter(function(e) {
+      return e.hasAttribute("data-pagedtable-source");
     });
 
-    return typedColumns;
+    if (sourceElems === null || sourceElems.length !== 1) {
+      throw("A single data-pagedtable-source was not found");
+    }
+
+    return JSON.parse(sourceElems[0].innerHTML);
+  }(pagedTable);
+
+  var Page = function(data) {
+    var me = this;
+
+    me.size = 10;
+    me.count = 0;
+    me.number = 0;
+
+    var getPageCount = function() {
+      return Math.ceil(data.length / me.size);
+    };
+
+    me.setPageNumber = function(newPageNumber) {
+      if (newPageNumber < 0) return;
+      if (newPageNumber * me.size >= data.length) return;
+
+      me.number = newPageNumber;
+    };
+
+    me.getVisiblePageRange = function() {
+      var start = me.number - 4;
+      var end = me.number + 5;
+
+      if (start < 0) {
+        var diffToStart = 0 - start;
+        start += diffToStart;
+        end += diffToStart;
+      }
+
+      if (end > me.count) {
+        var diffToEnd = end - me.count;
+        start -= diffToEnd;
+        end -= diffToEnd;
+      }
+
+      start = start < 0 ? 0 : start;
+      end = end >= me.count ? me.count : end;
+
+      return {
+        start: start,
+        end: end
+      };
+    };
+
+    me.count = getPageCount();
+  };
+
+  var Columns = function(source) {
+    var me = this;
+
+    me.number = 0;
+    me.visible = 10;
+    me.total = source.columns.length;
+    me.subset = [];
+
+    me.incColumnNumber = function(increment) {
+      var newColumnNumber = me.number + increment;
+      if (newColumnNumber + me.visible >= me.total)
+        newColumnNumber = me.total - me.visible;
+
+      if (newColumnNumber < 0) newColumnNumber = 0;
+
+      me.number = newColumnNumber;
+      me.subset = source.columns.slice(me.number, Math.min(me.number + me.visible, me.total));
+    };
+
+    me.incColumnNumber(0);
+    return me;
+  };
+
+  var data = source.data;
+  var page = new Page(data);
+  var columns = new Columns(source);
+
+  var renderColumnNavigation = function(increment) {
+    var header = document.createElement("th");
+    header.appendChild(document.createTextNode("..."));
+    header.setAttribute("style", "cursor: pointer;");
+
+    header.onclick = function() {
+      columns.incColumnNumber(increment);
+      renderHeader();
+      renderBody();
+    };
+
+    return header;
   };
 
   var renderHeader = function() {
@@ -36,7 +105,10 @@ var PagedTable = function (pagedTable) {
     var header = document.createElement("tr");
     thead.appendChild(header);
 
-    columnsData.forEach(function(columnData) {
+    if (columns.number > 0)
+      header.appendChild(renderColumnNavigation(-columns.visible));
+
+    columns.subset.forEach(function(columnData) {
       var column = document.createElement("th");
       column.setAttribute("align", columnData.align);
 
@@ -54,6 +126,9 @@ var PagedTable = function (pagedTable) {
       header.appendChild(column);
     });
 
+    if (columns.number + columns.visible < columns.total)
+      header.appendChild(renderColumnNavigation(columns.visible));
+
     return thead;
   };
 
@@ -61,20 +136,26 @@ var PagedTable = function (pagedTable) {
     var tbody = pagedTable.querySelectorAll("tbody")[0];
     tbody.innerHTML = "";
 
-    var pageData = data.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize);
+    var pageData = data.slice(page.number * page.size, (page.number + 1) * page.size);
 
     pageData.forEach(function(dataRow, idxRow) {
       var htmlRow = document.createElement("tr");
       htmlRow.setAttribute("class", (idxRow % 2 !==0) ? "even" : "odd");
 
-      columnsData.forEach(function(columnData) {
+      if (columns.number > 0)
+        htmlRow.appendChild(document.createElement("td"));
+
+      columns.subset.forEach(function(columnData) {
         var cellName = columnData.name;
-        var dataCell = cellName === "..." ? "" : dataRow[cellName];
+        var dataCell = dataRow[cellName];
         var htmlCell = document.createElement("td");
         htmlCell.appendChild(document.createTextNode(dataCell));
         htmlCell.setAttribute("align", columnData.align);
         htmlRow.appendChild(htmlCell);
       });
+
+      if (columns.number + columns.visible < columns.total)
+        htmlRow.appendChild(document.createElement("td"));
 
       tbody.appendChild(htmlRow);
     });
@@ -82,39 +163,14 @@ var PagedTable = function (pagedTable) {
     return tbody;
   };
 
-  var getVisiblePageRange = function() {
-    var start = pageNumber - 4;
-    var end = pageNumber + 5;
-
-    if (start < 0) {
-      var diffToStart = 0 - start;
-      start += diffToStart;
-      end += diffToStart;
-    }
-
-    if (end > pageCount) {
-      var diffToEnd = end - pageCount;
-      start -= diffToEnd;
-      end -= diffToEnd;
-    }
-
-    start = start < 0 ? 0 : start;
-    end = end >= pageCount ? pageCount : end;
-
-    return {
-      start: start,
-      end: end
-    };
-  };
-
   var getLabelInfo = function() {
-    var pageStart = pageNumber * pageSize + 1;
-    var pageEnd = Math.min((pageNumber + 1) * pageSize, data.length);
+    var pageStart = page.number * page.size + 1;
+    var pageEnd = Math.min((page.number + 1) * page.size, data.length);
     var totalRecods = data.length;
 
     var infoText = pageStart + "-" + pageEnd + " of " + totalRecods;
-    if (totalRecods < pageSize) {
-      infoText = totalRecods + " Records";
+    if (totalRecods < page.size) {
+      infoText = totalRecods + " Record" + (totalRecods != 1 ? "s" : "");
     }
 
     return infoText;
@@ -127,40 +183,40 @@ var PagedTable = function (pagedTable) {
     var next = document.createElement("a");
     next.appendChild(document.createTextNode("Next"));
     next.onclick = function() {
-      setPageNumber(pageNumber + 1);
-      renderBody(pagedTable);
-      renderFooter(pagedTable);
+      page.setPageNumber(page.number + 1);
+      renderBody();
+      renderFooter();
     };
-    if (data.length > pageSize) footer.appendChild(next);
+    if (data.length > page.size) footer.appendChild(next);
 
     var pageNumbers = document.createElement("div");
     pageNumbers.setAttribute("class", "pagedtable-indexes");
 
-    var pageRange = getVisiblePageRange();
+    var pageRange = page.getVisiblePageRange();
     for (var idxPage = pageRange.start; idxPage < pageRange.end; idxPage++) {
       var pageLink = document.createElement("a");
-      pageLinkClass = idxPage === pageNumber ? "pagedtable-index pagedtable-index-current" : "pagedtable-index";
+      pageLinkClass = idxPage === page.number ? "pagedtable-index pagedtable-index-current" : "pagedtable-index";
       pageLink.setAttribute("class", pageLinkClass);
       pageLink.setAttribute("data-page-index", idxPage);
       pageLink.onclick = function() {
-        setPageNumber(parseInt(this.getAttribute("data-page-index")));
-        renderBody(pagedTable);
-        renderFooter(pagedTable);
+        page.setPageNumber(parseInt(this.getAttribute("data-page-index")));
+        renderBody();
+        renderFooter();
       };
 
       pageLink.appendChild(document.createTextNode(idxPage + 1));
       pageNumbers.appendChild(pageLink);
     }
-    if (data.length > pageSize) footer.appendChild(pageNumbers);
+    if (data.length > page.size) footer.appendChild(pageNumbers);
 
     var previous = document.createElement("a");
     previous.appendChild(document.createTextNode("Previous"));
     previous.onclick = function() {
-      setPageNumber(pageNumber - 1);
+      page.setPageNumber(page.number - 1);
       renderBody(pagedTable);
       renderFooter(pagedTable);
     };
-    if (data.length > pageSize) footer.appendChild(previous);
+    if (data.length > page.size) footer.appendChild(previous);
 
     var infoLabel = document.createElement("div");
     infoLabel.setAttribute("class", "pagedtable-info");
@@ -169,33 +225,8 @@ var PagedTable = function (pagedTable) {
 
     var enabledClass = "pagedtable-index-nav";
     var disabledClass = "pagedtable-index-nav pagedtable-index-nav-disabled";
-    previous.setAttribute("class", pageNumber <= 0 ? disabledClass : enabledClass);
-    next.setAttribute("class", (pageNumber + 1) * pageSize >= data.length - 1 ? disabledClass : enabledClass);
-  };
-
-  var loadDataFromPagedTable = function() {
-    var sourceElems = [].slice.call(pagedTable.children).filter(function(e) {
-      return e.hasAttribute("data-pagedtable-source");
-    });
-
-    if (sourceElems === null || sourceElems.length !== 1) {
-      throw("A single data-pagedtable-source was not found");
-    }
-
-    source = JSON.parse(sourceElems[0].innerHTML);
-    data = source.data;
-    columnsData = columnsFromSource(source);
-  };
-
-  var setPageNumber = function(newPageNumber) {
-    if (newPageNumber < 0) return;
-    if (newPageNumber * pageSize >= data.length) return;
-
-    pageNumber = newPageNumber;
-  };
-
-  var getPageCount = function() {
-    return Math.ceil(data.length / pageSize);
+    previous.setAttribute("class", page.number <= 0 ? disabledClass : enabledClass);
+    next.setAttribute("class", (page.number + 1) * page.size >= data.length - 1 ? disabledClass : enabledClass);
   };
 
   this.render = function() {
@@ -224,13 +255,6 @@ var PagedTable = function (pagedTable) {
     renderBody();
     renderFooter();
   };
-
-  var init = function() {
-    loadDataFromPagedTable(pagedTable);
-    pageCount = getPageCount();
-  };
-
-  init();
 };
 
 var PagedTableDoc;
