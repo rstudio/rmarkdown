ace87ba215879c4660a38823e18cd05abbeaef46
JJ Allaire <jj@rstudio.org>
2016-11-17 08:50:44 -0500

    use dot for launch.browser delimiter

--- R/shiny.R
+++ R/shiny.R
@@ -181,8 +181,8 @@ run <- function(file = "index.Rmd", dir = dirname(file), default_file = NULL,
 
   # launch the app and open a browser to the requested page, if one was
   # specified
-  if (!is.null(shiny_args) && !is.null(shiny_args$launch_browser))
-    launch_browser <- shiny_args$launch_browser
+  if (!is.null(shiny_args) && !is.null(shiny_args$launch.browser))
+    launch_browser <- shiny_args$launch.browser
   else
     launch_browser <- (!is.null(file)) && interactive()
   if (isTRUE(launch_browser)) {
