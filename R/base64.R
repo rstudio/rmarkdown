86a6faad570551c2b619c953a83aeec218b925b7
Yihui Xie <xie@yihui.name>
2017-11-10 11:49:34 -0600

    use the mime package for MIME types

--- R/base64.R
+++ R/base64.R
@@ -1,96 +1,3 @@
-
-.MIMEMAP <- list(
-
-  ## most common web types
-  htm = 'text/html',
-  html = 'text/html',
-  css = 'text/css',
-  gif = 'image/gif',
-  jpg = 'image/jpeg',
-  jpeg = 'image/jpeg',
-  jpe = 'image/jpeg',
-  png = 'image/png',
-  js = 'application/x-javascript',
-  pdf = 'application/pdf',
-  svg = 'image/svg+xml',
-  swf = 'application/x-shockwave-flash',
-
-  ## markdown types
-  md = 'text/x-markdown',
-  mdtxt = 'text/x-markdown',
-  markdown = 'text/x-markdown',
-
-  ## other types we are likely to serve
-  xml = 'text/xml',
-  csv = 'text/csv',
-  ico = 'image/x-icon',
-  zip = 'application/zip',
-  bz = 'application/x-bzip',
-  bz2 = 'application/x-bzip2',
-  gz = 'application/x-gzip',
-  tar = 'application/x-tar',
-
-  ## yet more types...
-
-  shtml = 'text/html',
-  tsv = 'text/tab-separated-values',
-  tab = 'text/tab-separated-values',
-  dcf = 'text/debian-control-file',
-  txt = 'text/plain',
-  mml = 'text/mathml',
-
-  tif = 'image/tiff',
-  tiff = 'image/tiff',
-  bmp = 'image/bmp',
-  ps = 'application/postscript',
-  eps = 'application/postscript',
-  dvi =   'application/x-dvi',
-
-  atom = 'application/atom+xml',
-  rss = 'application/rss+xml',
-
-  doc = 'application/msword',
-  docx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-  odt = 'application/vnd.oasis.opendocument.text',
-  rtf = 'application/rtf',
-  xls = 'application/vnd.ms-excel',
-  xlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  ods = 'application/x-vnd.oasis.opendocument.spreadsheet',
-  ppt = 'application/vnd.ms-powerpoint',
-  pps = 'application/vnd.ms-powerpoint',
-  pptx = 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-
-  sit = 'application/x-stuffit',
-  sxw = 'application/vnd.sun.xml.writer',
-
-  iso = 'application/octet-stream',
-  dmg = 'application/octet-stream',
-  exe = 'application/octet-stream',
-  dll = 'application/octet-stream',
-  deb = 'application/octet-stream',
-  otf = 'application/octet-stream',
-  xpi = 'application/x-xpinstall',
-
-  mp2 = 'audio/mpeg',
-  mp3 = 'audio/mpeg',
-
-  mpg = 'video/mpeg',
-  mpeg = 'video/mpeg',
-  flv = 'video/x-flv'
-)
-
-
-mime_type <- function(f) {
-  f <- f[1]
-  file_ext <- function(x) {
-    pos <- regexpr('\\.([[:alnum:]]+)$', x)
-    ifelse(pos > -1L, tolower(substring(x, pos + 1L)), '')
-  }
-  ext <- file_ext(f)
-  ifelse(nchar(ext) > 1L && !is.null(.MIMEMAP[[ext]]), .MIMEMAP[[ext]], '')
-}
-
-
 base64_encode_file <- function(in_file, encoder) {
 
   file_size <- file.info(in_file)$size
@@ -99,9 +6,8 @@ base64_encode_file <- function(in_file, encoder) {
     warning(in_file, 'is empty!')
     return(in_file)
   }
-  paste( 'data:', mime_type(in_file), ';base64,',
-         encoder(readBin(in_file, 'raw', n = file_size)),
-         sep = '')
+  paste0('data:', mime::guess_type(in_file), ';base64,',
+         encoder(readBin(in_file, 'raw', n = file_size)))
 }
 
 # processes an HTML resource, given a regular expression that locates
