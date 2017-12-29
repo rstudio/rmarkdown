2bfe4fa58dea5ec7e5982f41660eb47ac51b89c4
Yihui Xie <xie@yihui.name>
2017-12-29 10:17:46 -0600

    download the tarball of pandoc instead

--- tools/install-pandoc.sh
+++ tools/install-pandoc.sh
@@ -2,8 +2,9 @@
 
 mkdir -p $HOME/bin
 cd $HOME/bin
-curl -Lo pandoc https://travis-bin.yihui.name/pandoc
-curl -Lo pandoc-citeproc https://travis-bin.yihui.name/pandoc-citeproc
+curl -Lo pandoc https://travis-bin.yihui.name/pandoc.tar.gz
+tar zxf pandoc.tar.gz
+rm pandoc.tar.gz
 chmod +x pandoc*
 pandoc --version
 pandoc-citeproc --version
