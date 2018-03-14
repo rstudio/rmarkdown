4d9ae2d9b9d89376fcc69e0ee860a7e6281c2b99
Richard Iannone <riannone@me.com>
2018-03-14 15:36:42 -0400

    Add @details sections to roxygen documentation
    Addresses issue #1252. Links to articles at `https://rmarkdown.rstudio.com/` (and other supporting information) are provided.

--- R/github_document.R
+++ R/github_document.R
@@ -1,19 +1,19 @@
 #' Convert to GitHub Flavored Markdown
 #'
 #' Format for converting from R Markdown to GitHub Flavored Markdown.
-#'
 #' @inheritParams output_format
 #' @inheritParams html_document
 #' @inheritParams md_document
-#'
 #' @param hard_line_breaks \code{TRUE} to generate markdown that uses a simple
 #'   newline to represent a line break (as opposed to two-spaces and a newline).
-#'
 #' @param html_preview \code{TRUE} to also generate an HTML file for the purpose of
 #'   locally previewing what the document will look like on GitHub.
-#'
+#' @details
+#' See the
+#' \href{http://rmarkdown.rstudio.com/github_document_format.html}{online
+#' documentation} for additional details on using the \code{github_document}
+#' format.
 #' @return R Markdown output format to pass to \code{\link{render}}
-#'
 #' @export
 github_document <- function(toc = FALSE,
                             toc_depth = 3,
