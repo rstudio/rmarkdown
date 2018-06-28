20bf1bc2466b3db833cd93e038e71140266dc69d
Yihui Xie <xie@yihui.name>
2018-06-28 10:58:25 -0500

    strip off /rmarkdown in the pathname

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,3 @@
 if (window.location.hostname === 'rstudio.github.io') {
-  window.location = 'https://rmarkdown.rstudio.com' + location.pathname;
+  window.location = 'https://rmarkdown.rstudio.com' + location.pathname.replace(/^\/rmarkdown/, '');
 }
