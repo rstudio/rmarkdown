02a87d69bf7653ca90b8a7c810e82a0519c7b336
Yihui Xie <xie@yihui.name>
2018-07-10 17:36:38 -0500

    add little icons &boxbox; after link text to indicate external links (thanks for the feedback from @pyltime)

--- js/external-links.js
+++ js/external-links.js
@@ -1,6 +1,17 @@
 (function() {
-  var links = document.getElementsByTagName('a');
+  var links = document.getElementsByTagName('a'), link, n, child;
   for (var i = 0; i < links.length; i++) {
-    if (/^(https?:)?\/\//.test(links[i].getAttribute('href'))) links[i].target = '_blank';
+    link = links[i];
+    if (/^(https?:)?\/\//.test(link.getAttribute('href'))) {
+      link.target = '_blank';
+      n = link.childElementCount;
+      if (n === 0 && link.innerText) {
+        link.innerHTML += ' &boxbox;';  // pure text
+      } else if (n === 2) {
+        // the page /formats.html
+        child = link.firstElementChild;
+        if (child.className === 'formatName') child.innerHTML += ' &boxbox;';
+      }
+    }
   }
 })();
