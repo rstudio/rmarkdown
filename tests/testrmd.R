9b609e7ad54d53cabdb5013af319814c50010222
Yihui Xie <xie@yihui.name>
2018-12-06 15:15:42 -0600

    add a test case for #1224 and #1288 (Travis should fail for this commit)

--- tests/testrmd.R
+++ tests/testrmd.R
@@ -6,4 +6,16 @@ if (.Platform$OS.type == 'unix' && !is.na(Sys.getenv('CI', NA))) {
   system2(rmarkdown::pandoc_exec(), '--print-default-data-file reference.docx', stdout = 'rmd/template.docx')
   rmarkdown::render('rmd/word.Rmd', intermediates_dir = 'tmp')
 
+  # use an absolute output_dir should not trigger a file.rename() warning
+  # (https://github.com/rstudio/rmarkdown/issues/1224)
+  withCallingHandlers(
+    rmarkdown::render(
+      'rmd/output_dir.Rmd', output_dir = file.path(tempdir(), 'output'),
+      intermediates_dir = file.path(tempdir(), 'tmp')
+    ), warning = function(e) {
+      if (identical(deparse(e$call), 'file.rename(from, to)') || grepl('cannot rename file', e$message)) {
+        stop(e)
+      }
+    }
+  )
 }
