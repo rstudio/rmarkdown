093c7bfec5141923e46716766e7281e2f3c4a63b
Yihui Xie <xie@yihui.name>
2018-12-06 23:45:09 -0600

    add a test case for #1358 (this should fail on Travis for the current HEAD version)

--- tests/testrmd.R
+++ tests/testrmd.R
@@ -6,6 +6,10 @@ if (.Platform$OS.type == 'unix' && !is.na(Sys.getenv('CI', NA))) {
   system2(rmarkdown::pandoc_exec(), '--print-default-data-file reference.docx', stdout = 'rmd/template.docx')
   rmarkdown::render('rmd/word.Rmd', intermediates_dir = 'tmp')
 
+  # bib files should be copied to the intermediates dir:
+  # https://github.com/rstudio/rmarkdown/issues/1358
+  rmarkdown::render('rmd/two-bibs.Rmd', intermediates_dir = tempdir())
+
   # use an absolute output_dir should not trigger a file.rename() warning
   # (https://github.com/rstudio/rmarkdown/issues/1224)
   withCallingHandlers(
