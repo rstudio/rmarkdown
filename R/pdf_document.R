f55a60b462c0f94e284e35f57411d747f6b40149
Andrew Dunning <andunning@gmail.com>
2019-04-04 13:34:06 -0400

    Missing quotes

--- R/pdf_document.R
+++ R/pdf_document.R
@@ -117,7 +117,7 @@ pdf_document <- function(toc = FALSE,
     if (version <= "2.5")
       args <- c(args, "--include-in-header",
                 pandoc_path_arg(rmarkdown_system_file(paste0("rmd/latex/",
-                                                             subtitle.tex))))
+                                                             "subtitle.tex"))))
 
   } else if (!is.null(template)) {
     args <- c(args, "--template", pandoc_path_arg(template))
