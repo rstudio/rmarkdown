a7dadc839346b6bfb3ec5429aa0cdfd9fafbdfe4
atusy <30277794+atusy@users.noreply.github.com>
2019-08-26 22:38:04 +0900

    doc

--- R/render.R
+++ R/render.R
@@ -152,6 +152,7 @@ NULL
 #' @seealso
 #' \link[knitr:knit]{knit}, \link{output_format},
 #' \href{http://johnmacfarlane.net/pandoc}{pandoc}
+#' @inheritParams default_output_format
 #' @param input The input file to be rendered. This can be an R script (.R),
 #' an R Markdown document (.Rmd), or a plain markdown document.
 #' @param output_format The R Markdown output format to convert to. The option
