65178ca384dd35286e25be6b577da5cdce7b3ed6
Yihui Xie <xie@yihui.name>
2019-10-31 16:39:34 -0500

    .btn-default -> .btn-secondary; .btn-xs -> .btn-sm; .pull-right -> .float-right

--- inst/rmd/h/navigation-1.1/codefolding.js
+++ inst/rmd/h/navigation-1.1/codefolding.js
@@ -31,7 +31,7 @@ window.initializeCodeFolding = function(show) {
 
     // add a show code button right above
     var showCodeText = $('<span>' + (show ? 'Hide' : 'Code') + '</span>');
-    var showCodeButton = $('<button type="button" class="btn btn-default btn-xs code-folding-btn pull-right"></button>');
+    var showCodeButton = $('<button type="button" class="btn btn-secondary btn-sm code-folding-btn float-right"></button>');
     showCodeButton.append(showCodeText);
     showCodeButton
         .attr('data-toggle', 'collapse')
