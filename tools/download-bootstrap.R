bab60bcd6f649f7667dd88872a65c10a3df3d9bd
Yihui Xie <xie@yihui.name>
2019-11-01 15:56:01 -0500

    download all themes from bootswatch.com, and replace the CSS @import with actual font specs so the themes can continue to work offline

--- tools/download-bootstrap.R
+++ tools/download-bootstrap.R
@@ -21,4 +21,58 @@ xfun::download_file(cdn('css/bootstrap.min.css'))
 # bootstrap.js + popper.js
 xfun::download_file(cdn('js/bootstrap.bundle.min.js'), '../js/bootstrap.min.js')
 
+# replace the CSS @import with actual CSS font specs so the themes can work offline
+local({
+  # font weights
+  w <- c(
+    '100' = 'Thin', '200' = 'ExtraLight', '300' = 'Light', '400' = 'Regular',
+    '500' = 'Medium', '600' = 'SemiBold', '700' = 'Bold', '800' = 'ExtraBold',
+    '900' = 'Black'
+  )
+  w <- c(w, setNames(paste(w, 'Italic'), paste0(names(w), 'italic')))
+  w['400italic'] <- 'Italic'
+  fonts <- list.files('fonts')
+  r <- '.*@import url\\("?https://fonts[.]googleapis[.]com/css[?]family=([^")]+)"?\\);.*'
+  for (f in list.files('.', '[.]min[.]css$')) {
+    x <- xfun::read_utf8(f)
+    s <- gsub(r, '\\1', grep(r, x, value = TRUE))
+    if (length(s) == 0) next
+    s <- gsub('+', ' ', s, fixed = TRUE)
+    css <- unlist(lapply(unlist(strsplit(s, '|', fixed = TRUE)), function(a) {
+      a <- unlist(strsplit(a, ':', fixed = TRUE))
+      if (length(a) > 2) stop('Something wrong with the font spec: ', s)
+      a <- c(a[1], if (length(a) == 1) '400' else unlist(strsplit(a[2], ',')))
+      css <- NULL; f1 <- a[1]; f2 <- gsub(' +', '', f1)
+      for (i in a[-1]) {
+        # normalize 400i to 400italic
+        i <- sub('^([0-9]+)i$', '\\1italic', i)
+        f3 <- paste(f2, gsub(' +', '', w[i]), sep = '-')
+        f4 <- sprintf('fonts/%s.ttf', f3)
+        if (!file.exists(f4)) stop(
+          'The font ', f4, ' does not exist. You need to download it.'
+        )
+        fonts <<- setdiff(fonts, basename(f4))
+        css <- c(css, c(
+          "@font-face{",
+          sprintf("font-family:'%s';", f1),
+          sprintf("font-style:%s;", if (grepl('italic', i)) 'italic' else 'normal'),
+          sprintf("font-weight:%s;", sub('italic', '', i)),
+          sprintf(
+            "src:local('%s'), local('%s'), url(%s) format('truetype');",
+            paste(f1, w[i]), f3, f4
+          ),
+          "}"
+        ))
+      }
+      css
+    }))
+    m <- gregexpr(gsub('^[.][*]|[.][*]$', '', r), x)
+    regmatches(x, m) <- list(paste(c('\n', css, '\n'), collapse = ''))
+    xfun::write_utf8(x, f)
+  }
+  if (length(fonts) > 0) warning(
+    'These fonts are no longer used: ', paste(fonts, collapse = ', ')
+  )
+})
+
 setwd(owd)
