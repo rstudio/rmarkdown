7db2966722b51bc7afd217350531e911a81d5525
Carson <cpsievert1@gmail.com>
2019-11-25 16:32:46 -0600

    Use new bootswatch_theme_navbar_height() function
    Added in https://github.com/rstudio/bootsass/commit/29a5967de51bccd91e0c743c8e5d67bc9b793cde

--- R/html_document.R
+++ R/html_document.R
@@ -550,31 +550,7 @@ pandoc_body_padding_variable_args <- function(theme, version) {
            yeti = 45,
            51)
   } else {
-    # TODO: it'd be great if, someday, this took into account SASS variables,
-    # but it's not immediately obvious what the model for that should be
-    switch(theme,
-           cerulean = 56,
-           cosmo = 54.5,
-           cyborg = 53,
-           darkly = 70.5,
-           flatly = 70.5,
-           journal = 61,
-           litera = 59.5,
-           lumen = 57,
-           lux = 91.25,
-           materia = 80.3,
-           minty = 56,
-           pulse = 75.4,
-           sandstone = 54,
-           simplex = 66.3,
-           sketchy = 58,
-           slate = 56.5,
-           solar = 56,
-           spacelab = 58,
-           superhero = 48,
-           united = 56,
-           yeti = 54.5,
-           60)
+    bootsass::bootswatch_theme_navbar_height(theme)
   }
 
   # header padding is bodyPadding + 5
