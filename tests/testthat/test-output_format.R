90abde73cda037171c45494885ea6cd9e7d516aa
Yihui Xie <xie@yihui.name>
2020-05-28 15:17:49 -0500

    = to <-

--- tests/testthat/test-output_format.R
+++ tests/testthat/test-output_format.R
@@ -1,24 +1,24 @@
 test_that("all elements can be NULL", {
-  out_fmt = output_format(
+  out_fmt <- output_format(
     knitr = NULL, pandoc = NULL, keep_md = NULL, clean_supporting = NULL
   )
   lapply(out_fmt, expect_null)
 })
 
 test_that("inherits base format", {
-  base_fmt = html_document()
-  out_fmt = output_format(
+  base_fmt <- html_document()
+  out_fmt <- output_format(
     knitr = NULL, pandoc = NULL, keep_md = NULL, clean_supporting = NULL,
     base_format = base_fmt
   )
-  classes = lapply(base_fmt, class)
+  classes <- lapply(base_fmt, class)
   expect_identical(lapply(out_fmt, class), classes)
-  not_fun = vapply(classes, function(x) !"function" %in% x, NA)
+  not_fun <- vapply(classes, function(x) !"function" %in% x, NA)
   expect_identical(out_fmt[not_fun], base_fmt[not_fun])
 })
 
 test_that("clean_supporting is coerced to FALSE only if keep_md is TRUE", {
-  args_combinations = expand.grid(
+  args_combinations <- expand.grid(
     knitr = list(NULL),
     pandoc = list(NULL),
     keep_md = list(TRUE, FALSE, NULL),
