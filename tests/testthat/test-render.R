93f4415a06eb1d23b24dc5c5e88811320fef5b98
atusy <30277794+atusy@users.noreply.github.com>
2020-07-05 00:24:16 +0900

    add test

---
+++ tests/testthat/test-render.R
@@ -0,0 +1,12 @@
+test_that("Metadata is available before pre_knit", {
+  message_pre_knit = 'pre_knit handles metadata'
+  fmt <- md_document()
+  fmt$pre_knit <- function(input, ...) {
+    if (!identical(metadata, list(foo = 'bar'))) {
+      message(message_pre_knit)
+    }
+  }
+  input_file = tempfile(fileext = '.md')
+  writeLines('---\nfoo: bar\n---', input_file)
+  testthat::expect_message(render(input_file, fmt), message_pre_knit)
+})
