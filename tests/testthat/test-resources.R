f4bddf8a65612e038eba609ab619a48e70bf76fd
atusy <30277794+atusy@users.noreply.github.com>
2020-07-20 21:57:30 +0900

    update tests

--- tests/testthat/test-resources.R
+++ tests/testthat/test-resources.R
@@ -5,15 +5,17 @@ test_that("R Markdown resource discovery finds expected resources", {
   resources <- find_external_resources("resources/rmarkdown.Rmd")
   expected <- data.frame(
     path = c("empty.md", "empty.png", "empty.tsv", "empty.Rmd", "empty.css",
-             "empty.jpg", "empty.html", "empty.csv"),
-    explicit = c(FALSE, FALSE, TRUE,  FALSE, FALSE, FALSE, FALSE, FALSE),
-    web      = c(FALSE, FALSE, FALSE, FALSE, TRUE,  TRUE,  TRUE,  FALSE),
+             "empty.jpg", "empty.html", "empty2.html", "empty.csv"),
+    explicit = c(FALSE, FALSE, TRUE,  FALSE, FALSE, FALSE, FALSE, FALSE, FALSE),
+    web      = c(FALSE, FALSE, FALSE, FALSE, TRUE,  TRUE,  TRUE,  TRUE,  FALSE),
     stringsAsFactors = FALSE)
 
   # sort by filename to avoid errors arising from file ordering -- we don't
   # really care what order these come back in
   resources <- as.data.frame(resources[order(resources[[1]]), , drop = FALSE])
   expected <- as.data.frame(expected[order(expected[[1]]), , drop = FALSE])
+  print(resources)
+  print(expected)
 
   expect_equal(resources, expected)
 })
