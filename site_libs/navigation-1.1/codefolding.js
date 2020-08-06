8394c2f3df193cf6bbebcc6c6a523f57e26e51a9
Yihui Xie <xie@yihui.name>
2020-08-05 23:31:06 -0500

    rebuild site

--- site_libs/navigation-1.1/codefolding.js
+++ site_libs/navigation-1.1/codefolding.js
@@ -17,7 +17,7 @@ window.initializeCodeFolding = function(show) {
   var currentIndex = 1;
 
   // select all R code blocks
-  var rCodeBlocks = $('pre.r, pre.python, pre.bash, pre.sql, pre.cpp, pre.stan, pre.julia');
+  var rCodeBlocks = $('pre.r, pre.python, pre.bash, pre.sql, pre.cpp, pre.stan, pre.julia, pre.foldable');
   rCodeBlocks.each(function() {
 
     // create a collapsable div to wrap the code in
