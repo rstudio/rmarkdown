6b7863e705f60bb11c68f2ffc3178342e742ac2e
atusy <30277794+atusy@users.noreply.github.com>
2020-08-26 22:37:35 +0900

    add comments

--- inst/rmd/lua/number-sections.lua
+++ inst/rmd/lua/number-sections.lua
@@ -13,8 +13,12 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+https://github.com/atusy/lua-filters/blob/master/lua/number-sections.lua
 ]]
 local attributes_free_formats = {
+  -- Improve readability of markdown formats by let them be attributes free
+  -- https://pandoc.org/MANUAL.html#general-options
   commonmark=1,
   commonmark_x=1,
   gfm=1, markdown_github=1,
