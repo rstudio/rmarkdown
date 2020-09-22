3601fc0c5c7352ec2eeb5b4a84600eb4006c3a24
Yihui Xie <xie@yihui.name>
2020-09-22 13:17:22 -0500

    revert 02a87d69bf to remove the double boxes since they confused @mine-cetinkaya-rundel

--- js/external-links.js
+++ js/external-links.js
@@ -1,17 +1,6 @@
 (function() {
-  var links = document.getElementsByTagName('a'), link, n, child;
+  var links = document.getElementsByTagName('a');
   for (var i = 0; i < links.length; i++) {
-    link = links[i];
-    if (/^(https?:)?\/\//.test(link.getAttribute('href'))) {
-      link.target = '_blank';
-      n = link.childElementCount;
-      if (n === 0 && link.innerText) {
-        link.innerHTML += ' &boxbox;';  // pure text
-      } else if (n === 2) {
-        // the page /formats.html
-        child = link.firstElementChild;
-        if (child.className === 'formatName') child.innerHTML += ' &boxbox;';
-      }
-    }
+    if (/^(https?:)?\/\//.test(links[i].getAttribute('href'))) links[i].target = '_blank';
   }
 })();
