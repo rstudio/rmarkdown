30c8fce9eedd1d956e19365bdbdb448d56864c91
Yihui Xie <xie@yihui.name>
2020-09-29 14:24:45 -0500

    make sure the rmarkdown/lua/ folder exists

--- R/util.R
+++ R/util.R
@@ -63,7 +63,7 @@ pkg_file_arg <- function(..., package = "rmarkdown") {
 #' # get a specific filter
 #' pkg_file_lua(c("pagebreak.lua", "latex_div.lua"))
 pkg_file_lua <- function(filters = NULL, package = "rmarkdown") {
-  lua_folder <- pkg_file("rmarkdown", "lua", package = package)
+  lua_folder <- pkg_file("rmarkdown", "lua", package = package, mustWork = TRUE)
   if (is.null(filters)) filters <- list.files(lua_folder, "[.]lua$")
   pandoc_path_arg(file.path(lua_folder, filters))
 }
