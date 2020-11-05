fa59bd157f37c09c10dee7a7bdb18aab88496829
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-11-05 15:59:45 +0100

    new helper to get less wrapping in tests

--- tests/testthat/test-pandoc.R
+++ tests/testthat/test-pandoc.R
@@ -1,6 +1,10 @@
 test_that("Correct syntax highlighting argument as requested", {
   # helpers
-  highlight_style <- function(name = NULL) {
+  hl_args <- function(highlight, template, downlit = FALSE) {
+    pandoc_html_highlight_args(template = template, highlight = highlight,
+                               highlight_downlit = downlit)
+  }
+  hl_style <- function(name = NULL) {
     if (is.null(name))
       "--no-highlight"
     else
@@ -8,28 +12,16 @@ test_that("Correct syntax highlighting argument as requested", {
   }
   downlit  <- pandoc_variable_arg("highlight-downlit=1")
   highlightjs <- pandoc_variable_arg("highlightjs=1")
-  a11y_theme <- highlight_style(pkg_file_highlight("a11y.theme"))
+  a11y_theme <- hl_style(pkg_file_highlight("a11y.theme"))
   # check logic
-  expect_equal(pandoc_html_highlight_args("default", NULL), highlight_style())
-  expect_equal(pandoc_html_highlight_args("dummy.html", NULL), highlight_style())
-  expect_equal(pandoc_html_highlight_args("dummy.html", "default"),
-               highlight_style("pygments"))
-  expect_equal(pandoc_html_highlight_args("dummy.html", "zenburn"),
-               highlight_style("zenburn"))
-  expect_equal(pandoc_html_highlight_args("default", "breezedark"),
-               highlight_style("breezedark"))
-  expect_equal(pandoc_html_highlight_args("default", "default"),
-               c(highlight_style(), highlightjs))
-  expect_equal(pandoc_html_highlight_args("default", "default",
-                                          highlight_downlit = TRUE),
-               c(a11y_theme, downlit))
-  expect_equal(pandoc_html_highlight_args("dummy.html", "default",
-                                          highlight_downlit = TRUE),
-               c(a11y_theme, downlit))
-  expect_equal(pandoc_html_highlight_args("dummy.html", "textmate",
-                                          highlight_downlit = TRUE),
-               c(a11y_theme, downlit))
-  expect_equal(pandoc_html_highlight_args("default", "tango",
-                                          highlight_downlit = TRUE),
-               c(highlight_style("tango"),downlit))
+  expect_equal(hl_args(NULL, "default"), hl_style())
+  expect_equal(hl_args(NULL, "dummy.html"), hl_style())
+  expect_equal(hl_args("default", "dummy.html"), hl_style("pygments"))
+  expect_equal(hl_args("zenburn", "dummy.html"), hl_style("zenburn"))
+  expect_equal(hl_args("breezedark", "default"), hl_style("breezedark"))
+  expect_equal(hl_args("default", "default"), c(hl_style(), highlightjs))
+  expect_equal(hl_args("default", "default", TRUE), c(a11y_theme, downlit))
+  expect_equal(hl_args("default", "dummy.html", TRUE), c(a11y_theme, downlit))
+  expect_equal(hl_args("textmate", "dummy.html", TRUE), c(a11y_theme, downlit))
+  expect_equal(hl_args("tango", "default", TRUE), c(hl_style("tango"), downlit))
 })
