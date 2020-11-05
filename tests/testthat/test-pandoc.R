a3dfe4a766112674fb46fb00939af33c3bf2fa45
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-11-05 12:31:14 +0100

    and update test

--- tests/testthat/test-pandoc.R
+++ tests/testthat/test-pandoc.R
@@ -8,6 +8,7 @@ test_that("Correct syntax highlighting argument as requested", {
   }
   downlit  <- pandoc_variable_arg("highlight-downlit=1")
   highlightjs <- pandoc_variable_arg("highlightjs=1")
+  a11y_theme <- highlight_style(pkg_file_highlight("a11y.theme"))
   # check logic
   expect_equal(pandoc_html_highlight_args("default", NULL), highlight_style())
   expect_equal(pandoc_html_highlight_args("dummy.html", NULL), highlight_style())
@@ -21,13 +22,13 @@ test_that("Correct syntax highlighting argument as requested", {
                c(highlight_style(), highlightjs))
   expect_equal(pandoc_html_highlight_args("default", "default",
                                           highlight_downlit = TRUE),
-               c(highlight_style("pygments"), downlit))
+               c(a11y_theme, downlit))
   expect_equal(pandoc_html_highlight_args("dummy.html", "default",
                                           highlight_downlit = TRUE),
-               c(highlight_style("pygments"), downlit))
+               c(a11y_theme, downlit))
   expect_equal(pandoc_html_highlight_args("dummy.html", "textmate",
                                           highlight_downlit = TRUE),
-               c(highlight_style("pygments"), downlit))
+               c(a11y_theme, downlit))
   expect_equal(pandoc_html_highlight_args("default", "tango",
                                           highlight_downlit = TRUE),
                c(highlight_style("tango"),downlit))
