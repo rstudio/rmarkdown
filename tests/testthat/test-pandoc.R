fab4098596bce5e5da0248440b16f112fc6ddcfe
Christophe Dervieux <christophe.dervieux@gmail.com>
2020-11-05 18:08:32 +0100

    there is a wrapper for pandoc highlight arg already

--- tests/testthat/test-pandoc.R
+++ tests/testthat/test-pandoc.R
@@ -1,27 +1,39 @@
-test_that("Correct syntax highlighting argument as requested", {
+test_that("build highlight args for pandoc correctly", {
+  hl_style <- function(name) c("--highlight-style", name)
+  expect_equal(pandoc_highlight_args(NULL), "--no-highlight")
+  expect_equal(pandoc_highlight_args("default"), hl_style("tango"))
+  expect_equal(pandoc_highlight_args("default", "zenburn"), hl_style("zenburn"))
+  expect_equal(pandoc_highlight_args("zenburn"), hl_style("zenburn"))
+})
+
+
+test_that("Correct HTML highlighting argument as requested", {
   # helpers
   hl_args <- function(highlight, template, downlit = FALSE) {
     pandoc_html_highlight_args(template = template, highlight = highlight,
                                highlight_downlit = downlit)
   }
-  hl_style <- function(name = NULL) {
-    if (is.null(name))
-      "--no-highlight"
-    else
-      c("--highlight-style", name)
-  }
+  hl_style <- pandoc_highlight_args
   downlit  <- pandoc_variable_arg("highlight-downlit=1")
   highlightjs <- pandoc_variable_arg("highlightjs=1")
   a11y_theme <- hl_style(pkg_file_highlight("a11y.theme"))
   # check logic
-  expect_equal(hl_args(NULL, "default"), hl_style())
-  expect_equal(hl_args(NULL, "dummy.html"), hl_style())
+  expect_equal(hl_args(NULL, "default"), hl_style(NULL))
+  expect_equal(hl_args(NULL, "dummy.html"), hl_style(NULL))
   expect_equal(hl_args("default", "dummy.html"), hl_style("pygments"))
   expect_equal(hl_args("zenburn", "dummy.html"), hl_style("zenburn"))
   expect_equal(hl_args("breezedark", "default"), hl_style("breezedark"))
-  expect_equal(hl_args("default", "default"), c(hl_style(), highlightjs))
+  expect_equal(hl_args("default", "default"), c(hl_style(NULL), highlightjs))
   expect_equal(hl_args("default", "default", TRUE), c(a11y_theme, downlit))
   expect_equal(hl_args("default", "dummy.html", TRUE), c(a11y_theme, downlit))
   expect_equal(hl_args("textmate", "dummy.html", TRUE), c(a11y_theme, downlit))
   expect_equal(hl_args("tango", "default", TRUE), c(hl_style("tango"), downlit))
+})
+
+test_that("detect highlightjs theme", {
+  expect_false(is_highlightjs(NULL))
+  expect_false(is_highlightjs("zenburn"))
+  expect_false(is_highlightjs("default"))
+  expect_false(is_highlightjs("path/to/hl.theme"))
+  expect_true(is_highlightjs("textmate"))
 })
