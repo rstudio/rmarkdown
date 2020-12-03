c2df95e481613795dc24e3c84d61cf0f3273d194
christophe dervieux <christophe.dervieux@gmail.com>
2020-12-03 19:40:15 +0100

    add a unit test

---
+++ tests/testthat/test-html_document_base.R
@@ -0,0 +1,5 @@
+test_that("default email obfuscation parameter is used", {
+  skip_if_not(pandoc_available("1.17.2"))
+  args <- html_document_base()$pandoc$args
+  expect_false("--email-obfuscation" %in% args)
+})
