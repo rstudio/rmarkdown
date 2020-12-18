433e726ff5aee9cd14a285f756af814c44929c8d
J.J. Allaire <jj@rstudio.com>
2020-12-18 09:54:45 -0500

    Improvements to latex-div (#1984)

--- inst/rmarkdown/lua/latex-div.lua
+++ inst/rmarkdown/lua/latex-div.lua
@@ -4,29 +4,43 @@
      License: Public domain
 --]]
 
-Div = function (div)
-  local options = div.attributes['data-latex']
-  if options == nil then return nil end
+text = require 'text'
 
-  -- if the output format is not latex, remove the data-latex attr and return
+Div = function (div)
+  -- look for 'latex' or 'data-latex' and at least 1 class
+  local options = div.attributes['latex']
+  if not options then
+    options = div.attributes['data-latex']
+  end
+  if not options or #div.attr.classes == 0 then
+    return nil
+  end
+
+  -- if the output format is not latex, remove the attr and return
   if FORMAT ~= 'latex' and FORMAT ~= 'beamer' then
+    div.attributes['latex'] = nil
     div.attributes['data-latex'] = nil
     return div
   end
 
-  local env = div.classes[1]
-  -- if the div has no class, the object is left unchanged
-  if not env then return nil end
+  -- if it's "1" or "true" then just set it to empty string
+  if options == "1" or text.lower(options) == "true" then
+    options = ""
+  end
 
-  -- insert raw latex before content
-  table.insert(
-    div.content, 1,
-    pandoc.RawBlock('tex', '\\begin' .. '{' .. env .. '}' .. options)
-  )
-  -- insert raw latex after content
-  table.insert(
-    div.content,
-    pandoc.RawBlock('tex', '\\end{' .. env .. '}')
-  )
+  -- environment begin/end
+  local env = div.classes[1]
+  local beginEnv = '\\begin' .. '{' .. env .. '}' .. options
+  local endEnv = '\n\\end{' .. env .. '}'
+
+  -- if the first and last div blocks are paragraphs then we can
+  -- bring the environment begin/end closer to the content
+  if div.content[1].t == "Para" and div.content[#div.content].t == "Para" then
+    table.insert(div.content[1].content, 1, pandoc.RawInline('tex', beginEnv .. "\n"))
+    table.insert(div.content[#div.content].content, pandoc.RawInline('tex', "\n" .. endEnv))
+  else
+    table.insert(div.content, 1, pandoc.RawBlock('tex', beginEnv))
+    table.insert(div.content, pandoc.RawBlock('tex', endEnv))
+  end
   return div
 end
