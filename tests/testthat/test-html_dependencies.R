56c492b862c5110141d2d2f6e55a0d50a041b978
christophe dervieux <christophe.dervieux@gmail.com>
2020-12-21 17:43:58 +0100

    more explicit test

--- tests/testthat/test-html_dependencies.R
+++ tests/testthat/test-html_dependencies.R
@@ -1,15 +1,8 @@
 test_that("Correct anchor_sections style is used", {
   deps <- html_dependency_anchor_sections
   expect_s3_class(deps(), "html_dependency")
-  expect_error(deps("dummy"))
-  expect_match(deps()$stylesheet[[2]], "hash")
+  expect_error(deps("dummy"), "should be one of")
+  expect_match(deps()$stylesheet[[2]], "anchor-sections-hash.css")
   expect_null(deps()$script[[1]])
   expect_equal(deps(section_divs = TRUE)$script[[1]], "anchor-sections.js")
-  css <- function(style, id = 2) {
-    deps(style)$stylesheet[[id]]
-  }
-  expect_equal(css("hash", 1), "anchor-sections.css")
-  expect_equal(css("icon",1), "anchor-sections.css")
-  expect_match(css("icon"), "icon")
-  expect_match(css("symbol"), "symbol")
 })
