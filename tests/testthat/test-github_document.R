95afb2e3fbadc93a48d906b3b48c38bba0f6c07a
christophe dervieux <christophe.dervieux@gmail.com>
2021-02-09 12:58:07 +0100

    snapshot is different with pandoc versions
    New gfm writer I believe

--- tests/testthat/test-github_document.R
+++ tests/testthat/test-github_document.R
@@ -1,18 +1,20 @@
 
-local_edition(3)
-
 test_that("toc has correct identifier", {
+  local_edition(3)
   skip_on_cran() # avoid pandoc issue on CRAN
   tmp_file <- withr::local_tempfile()
   xfun::write_utf8(
     c("# 10 Section","","Sentence.","", "# Header","","Sentence "),
     tmp_file)
   res <- render(tmp_file, github_document(toc = TRUE, html_preview = FALSE))
-  expect_snapshot_file(res, "github-toc.md", binary = FALSE)
+  if (pandoc_available("2.10.1")) {
+    expect_snapshot_file(res, "github-toc.md", binary = FALSE)
+  }
 })
 
 
 test_that("github_document produces atx-header", {
+  local_edition(3)
   skip_on_cran() # avoid pandoc issue on CRAN
   tmp_file <- withr::local_tempfile()
   h <- paste0(Reduce(paste0, rep("#", 5), accumulate = TRUE), " title ", 1:5, "\n\n")
