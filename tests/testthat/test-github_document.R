2296fdac51736e66a3c27ed96e17f82f43070a5e
christophe dervieux <christophe.dervieux@gmail.com>
2021-02-09 12:07:03 +0100

    Add another simple header to test

--- tests/testthat/test-github_document.R
+++ tests/testthat/test-github_document.R
@@ -4,10 +4,9 @@ local_edition(3)
 test_that("toc has correct identifier", {
   skip_on_cran() # avoid pandoc issue on CRAN
   tmp_file <- withr::local_tempfile()
-  xfun::write_utf8(c(
-  "# 10 Section",
-  "",
-  "Sentence."), tmp_file)
+  xfun::write_utf8(
+    c("# 10 Section","","Sentence.","", "# Header","","Sentence "),
+    tmp_file)
   res <- render(tmp_file, github_document(toc = TRUE, html_preview = FALSE))
   expect_snapshot_file(res, "github-toc.md", binary = FALSE)
 })
