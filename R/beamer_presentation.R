a11240d78928a31d7e8aef3312709772715b8e91
Devon Ryan <dpryan79@users.noreply.github.com>
2021-03-24 17:26:47 +0100

    Add support for tectonic as a latex engine (#2078)

--- R/beamer_presentation.R
+++ R/beamer_presentation.R
@@ -111,7 +111,7 @@ beamer_presentation <- function(toc = FALSE,
   args <- c(args, pandoc_highlight_args(highlight))
 
   # latex engine
-  latex_engine <- match.arg(latex_engine, c("pdflatex", "lualatex", "xelatex"))
+  latex_engine <- match.arg(latex_engine, c("pdflatex", "lualatex", "xelatex", "tectonic"))
   args <- c(args, pandoc_latex_engine_args(latex_engine))
 
   # citation package
