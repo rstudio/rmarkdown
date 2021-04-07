d10adb8c300a1c9dd09e7702704e8aed6f589e97
Jiri Novotny <fg-42@seznam.cz>
2021-04-07 10:29:43 +0200

    Use the internal function move_dir()

--- R/render.R
+++ R/render.R
@@ -989,7 +989,7 @@ render <- function(input,
     if (!is.null(intermediates_dir)) {
       intermediate_output <- file.path(intermediates_dir, basename(output_file))
       if (file.exists(intermediate_output)) {
-        file.copy(intermediate_output, output_file) && unlink(intermediate_output)
+        move_dir(intermediate_output, output_file)
       }
     }
 
