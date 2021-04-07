1f70a5253cc0282292c9bf3d09371ad86dffc066
christophe dervieux <christophe.dervieux@gmail.com>
2021-04-07 17:41:42 +0200

    Add authors to pagebreak.lua

--- inst/rmarkdown/lua/pagebreak.lua
+++ inst/rmarkdown/lua/pagebreak.lua
@@ -2,6 +2,7 @@
 pagebreak – convert raw LaTeX page breaks to other formats
 
 Copyright © 2017-2019 Benct Philip Jonsson, Albert Krewinkel
+Copyright © 2019-2021 Christophe Dervieux, Romain Lesur, Atsushi Yasumoto
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
