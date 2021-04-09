869e6b50e30fc01a7cfde6765fb7810a0d8fadfc
Christophe Dervieux <cderv@rstudio.com>
2021-04-09 18:41:33 +0200

    use is.na instead of !nzchar
    Co-authored-by: Yihui Xie <xie@yihui.name>

--- R/pandoc.R
+++ R/pandoc.R
@@ -750,7 +750,7 @@ pandoc_citeproc <- function() {
 pandoc_lua_filter_args <- function(lua_files) {
   # Lua filters was introduced in pandoc 2.0
   if (pandoc2.0()) {
-    if (!nzchar(Sys.getenv("RMARKDOWN_LUA_SHARED"))) {
+    if (is.na(Sys.getenv("RMARKDOWN_LUA_SHARED", NA))) {
       Sys.setenv(RMARKDOWN_LUA_SHARED = pkg_file_lua("shared.lua"))
     }
     c(rbind("--lua-filter", pandoc_path_arg(lua_files)))
