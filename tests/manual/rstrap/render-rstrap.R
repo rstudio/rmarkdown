79641729bbeab06b02fb0c155f72f7e511bf9cb1
christophe dervieux <christophe.dervieux@gmail.com>
2021-05-13 12:48:38 +0200

    Add manual test to check rstrap is working

---
+++ tests/manual/rstrap/render-rstrap.R
@@ -0,0 +1,24 @@
+# TO RUN IN A CLEAN SESSION
+# install packages in a temp lib
+withr::local_temp_libpaths(action = "replace")
+.libPaths()
+install.packages("pak", repos = "https://r-lib.github.io/p/pak/dev/")
+pak::local_install_dev_deps(".")
+pak::local_install(".")
+
+# remove boostrap folder
+bs_folder <- system.file("rmd/h/bootstrap", package = "rmarkdown", lib.loc = .libPaths()[1])
+dir.exists(bs_folder)
+unlink(bs_folder, recursive = TRUE)
+dir.exists(bs_folder)
+
+# check rstrap is used and not bootstrap
+library(rmarkdown,lib.loc = .libPaths()[1])
+withr::local_dir("tests/manual/rstrap/")
+render("rstrap.Rmd", html_document(self_contained = FALSE))
+render("rstrap.Rmd", html_document(self_contained = FALSE, toc = TRUE, toc_float = TRUE))
+render("rstrap.Rmd", html_document(self_contained = FALSE, code_download = TRUE))
+render("rstrap.Rmd", html_document(self_contained = FALSE, code_folding = "hide"))
+
+# reset the environment
+withr::deferred_run()
