1b906461d0569daddeb6447d0907c3e628b274bc
christophe dervieux <christophe.dervieux@gmail.com>
2021-08-18 10:13:15 +0200

    Add tests

---
+++ tests/testthat/test-pandoc.R
@@ -0,0 +1,9 @@
+# TODO: to remove when switching the package to edition 3
+local_edition(3)
+
+test_that("Converting bib file is working", {
+  bib_file <- test_path("resources/UTF8.bib")
+  expect_snapshot_value(pandoc_citeproc_convert(bib_file, "list"), style = "deparse")
+  expect_snapshot_output(pandoc_citeproc_convert(bib_file, "json"))
+  expect_snapshot_output(pandoc_citeproc_convert(bib_file, "yaml"))
+})
