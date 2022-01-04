a3636bbe512023837ba5e218c83f3cb20da116fb
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-01-04 19:31:54 +0100

    Revert "remvoe regex when testing error"
    This reverts commit 193ac2d814f819cc7019679852f67b72d757abe2.

--- tests/testthat/test-html_dependencies.R
+++ tests/testthat/test-html_dependencies.R
@@ -4,7 +4,7 @@ local_edition(3)
 test_that("Correct anchor_sections style is used", {
   deps <- html_dependency_anchor_sections
   expect_s3_class(deps(), "html_dependency")
-  expect_error(deps("dummy"))
+  expect_error(deps("dummy"), "should be one of")
   expect_match(deps()$stylesheet[[2]], "anchor-sections-hash.css")
   expect_null(deps()$script[[1]])
   expect_equal(deps(section_divs = TRUE)$script[[1]], "anchor-sections.js")
