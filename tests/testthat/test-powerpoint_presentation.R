56243642f7dad67613131afe61b08459e02651dc
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-01-17 18:56:12 +0100

    We need to skip also in the incremental feature test

--- tests/testthat/test-powerpoint_presentation.R
+++ tests/testthat/test-powerpoint_presentation.R
@@ -4,6 +4,7 @@ test_that("Incremental feature", {
     ppt_out <- powerpoint_presentation(incremental = TRUE)
     expect_true("--incremental" %in% ppt_out$pandoc$args)
   } else {
+    skip_if_not_pandoc("2.0.5")
     expect_warning(ppt_out <- powerpoint_presentation(incremental = TRUE),
                    "is supported since Pandoc 2.15", fixed = TRUE)
     expect_false("--incremental" %in% ppt_out$pandoc$args)
