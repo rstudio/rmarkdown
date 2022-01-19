0fe16cef568ac516482f9f6e66905463b66c409f
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-01-19 10:18:06 +0100

    Update test

--- tests/testthat/test-draft.R
+++ tests/testthat/test-draft.R
@@ -1,5 +1,8 @@
 test_that("available_templates() correctly returns template", {
-  expect_null(available_templates("donotexist"))
+  expect_identical(
+    expect_invisible(available_templates("donotexist")),
+    character()
+  )
   expect_identical(available_templates(), dir(pkg_file("rmarkdown/templates")))
   expect_identical(available_templates(full_path = TRUE),
                    dir(pkg_file("rmarkdown/templates"), full.names = TRUE))
