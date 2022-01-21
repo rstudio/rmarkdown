c9679b515920f92398c6a818f835f42fdd85f11e
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-01-21 14:37:09 +0100

    Add comment and be more explicit on how the filter works
    It is quite complex filter in its logic as pre processing is done only for a subset of format.

--- inst/rmarkdown/lua/number-sections.lua
+++ inst/rmarkdown/lua/number-sections.lua
@@ -43,84 +43,105 @@ end
 
 
 function is_preprocess(meta)
+  -- Check if the filter is run in preprocessing step
+  -- This should only be done for markdown document conversion
   preprocess = meta['preprocess_number_sections']
+  return nil
 end
 
 
 function fix_link_id(link)
-  if not preprocess and identifiers[link.target] then
+  -- Some markdown formats needs fixing on there link to numbered sections
+  -- idenfiers is filled during processing when a pre processing has been run
+  if not preprocess and #identifiers and identifiers[link.target] then
     link.target = identifiers[link.target]
     return link
   end
+  -- do nothing in all other cases
+  return nil
 end
 
 
 function number_sections(header)
 
-  -- This will only run on second conversion to help fix the links in the TOC
-  -- by retrieving the recorded attributes
-  if not preprocess then
-    if #header.content and (header.content[1].t == 'Span') then
-      -- retrieve the attributes
+  ----------------------------
+  -- specific handling when pre-processing has been done
+  ----------------------------
+
+  -- This will only run on second conversion when a preprocessing has be run before
+  -- to add recorded attributes set by us as Span attribute in first processing.
+  if not preprocess and #header.content and (header.content[1].t == 'Span') then
+      -- Check attributes
       local span_content = header.content[1]
       local old_id = span_content.attributes['data-rmarkdown-temporarily-recorded-id']
+      -- If found, this is our Span added in pre processing step
       if old_id then
         identifiers['#' .. old_id] = '#' .. header.identifier
+        -- remove span
+        header.content:remove(1)
+        -- prepend span Inlines content to the header content
+        header.content = span_content.content:__concat(header.content)
+        -- return content without the span
+        return header
       end
-      -- remove span
-      header.content:remove(1)
-      -- prepend span Inlines content to the header content
-      header.content = span_content.content:__concat(header.content)
-      -- return content without the span
-      return header
-    end
-  else
-
-    -- The following part will run during the pre-processing step only to
-    --  * add numbers on header
-    --  * record the old identifier for next processing to fix links
-
-    -- Unnumbered
-    if header.classes:find("unnumbered") then
+      -- Otherwise, not our spans so do nothing
       return nil
+  end
+
+  -- The following part runs in pre-processing step or main processing step
+  -- depending on the format.
+  -- (md output require a pre processing and numbered section ared added on input).
+
+  -- If explicitly unnumbered, then we do nothing
+  if header.classes:find("unnumbered") then
+    return nil
+  end
+
+  ----------------------------
+  -- Adding the number section
+  ----------------------------
+
+  --- Reset and update section_number_table
+  if (header.level < previous_header_level) then
+    for i=header.level+1,n_section_number_table do
+      section_number_table[i] = 0
     end
+  end
+  previous_header_level = header.level
+  section_number_table[header.level] = section_number_table[header.level] + 1
 
-    --- Reset and update section_number_table
-    if (header.level < previous_header_level) then
-      for i=header.level+1,n_section_number_table do
-        section_number_table[i] = 0
-      end
+  --- Define section number as string
+  local section_number_string = tostring(section_number_table[header.level])
+  if header.level > 1 then
+    for i = header.level-1,1,-1 do
+      section_number_string = section_number_table[i] .. "." .. section_number_string
     end
-    previous_header_level = header.level
-    section_number_table[header.level] = section_number_table[header.level] + 1
+  end
 
-    --- Define section number as string
-    local section_number_string = tostring(section_number_table[header.level])
-    if header.level > 1 then
-      for i = header.level-1,1,-1 do
-        section_number_string = section_number_table[i] .. "." .. section_number_string
-      end
-    end
+  --- Update Header element
+  table.insert(header.content, 1, separator)
+  table.insert(header.content, 1, pandoc.Str(section_number_string))
 
-    --- Update Header element
-    table.insert(header.content, 1, separator)
-    table.insert(header.content, 1, pandoc.Str(section_number_string))
+  ----------------------------
+  -- Pre-processing specific handling
+  ----------------------------
 
-    --- Return Header wrapped in Spans with recording Header's current ID as attribute
+  --- Return Header wrapped in Spans with recording Header's current ID as attribute
+  if preprocess then
     local attr = {}
     attr['data-rmarkdown-temporarily-recorded-id'] = header.identifier
     header.content = {pandoc.Span(
       header.content,
       pandoc.Attr('', '', attr)
     )}
-
-    return header
   end
+
+  return header
 end
 
 
 return {
-  {Meta = is_preprocess},
-  {Header = number_sections},
-  {Link = fix_link_id},
+  {Meta = is_preprocess}, -- Are we running as pre-pocessing ?
+  {Header = number_sections}, -- Add Number to section
+  {Link = fix_link_id}, -- Only done when preprocessing has been run
 }
