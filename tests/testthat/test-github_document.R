f2e60d44e2d8cea36945c0d9b3dd33157c199ece
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-01-21 14:43:06 +0100

    Add test for number_section + toc on github_document

--- tests/testthat/test-github_document.R
+++ tests/testthat/test-github_document.R
@@ -1,23 +1,33 @@
+# TODO: to remove when switching the package to edition 3
+local_edition(3)
 
 test_that("toc has correct identifier", {
-  local_edition(3)
   skip_on_cran() # avoid pandoc issue on CRAN
   skip_if_not_pandoc("2.10.1") # changes in gfm writer break this test for earlier versions
   tmp_file <- local_rmd_file(
     c("# 10 Section","","Sentence.","", "# Header","","Sentence ")
   )
   res <- render(tmp_file, github_document(toc = TRUE, html_preview = FALSE))
-  expect_snapshot_file(res, "github-toc.md", binary = FALSE)
+  expect_snapshot_file(res, "github-toc.md", compare = compare_file_text)
+})
+
+test_that("toc has correct identifier also when sections are numbered ", {
+  skip_on_cran() # avoid pandoc issue on CRAN
+  skip_if_not_pandoc("2.10.1") # changes in gfm writer break this test for earlier versions
+  tmp_file <- local_rmd_file(
+    c("# Section","","Sentence.","", "# Header","","Sentence ")
+  )
+  res <- render(tmp_file, github_document(toc = TRUE, number_sections = TRUE, html_preview = FALSE))
+  expect_snapshot_file(res, "github-toc-numbered.md", compare = compare_file_text)
 })
 
 
 test_that("github_document produces atx-header", {
-  local_edition(3)
   skip_on_cran() # avoid pandoc issue on CRAN
   h <- paste0(Reduce(paste0, rep("#", 5), accumulate = TRUE), " title ", 1:5, "\n\n")
   tmp_file <- local_rmd_file(h)
   res <- render(tmp_file, github_document(html_preview = FALSE))
-  expect_snapshot_file(res, "github-atx.md", binary = FALSE)
+  expect_snapshot_file(res, "github-atx.md", compare = compare_file_text)
 })
 
 
