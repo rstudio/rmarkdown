0182690615ebf17aa19d8d2c5bd0b86baf049f56
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-05-24 18:42:23 +0200

    Add some tests

--- tests/testthat/test-github_document.R
+++ tests/testthat/test-github_document.R
@@ -44,4 +44,30 @@ test_that("github_document produces atx-header", {
   expect_snapshot_file(res, "github-atx.md", compare = compare_file_text)
 })
 
+test_that("github_document math support with native math", {
+  skip_on_cran()
+  skip_if_not_pandoc()
+  tmp_file <- local_rmd_file(c(
+    "**The Cauchy-Schwarz Inequality**",
+    "$$\\sum_{k=1}^n$$",
+    "",
+    "This sentence uses `$` delimiters to show math inline:  $\\sqrt{3x-1}+(1+x)^2$"
+  ))
+  res <- .render_and_read(tmp_file, output_format = github_document(html_preview = FALSE))
+  expect_match(res, "$$\\sum_{k=1}^n$$", fixed = TRUE, all = FALSE)
+  expect_match(res, "$\\sqrt{3x-1}+(1+x)^2$", fixed = TRUE, all = FALSE)
+})
 
+test_that("github_document math support with webtex math math", {
+  skip_on_cran()
+  skip_if_not_pandoc()
+  skip_if_offline("latex.codecogs.com")
+  tmp_file <- local_rmd_file(c(
+    "**The Cauchy-Schwarz Inequality**",
+    "$$\\sum_{k=1}^n$$",
+    "",
+    "This sentence uses `$` delimiters to show math inline:  $\\sqrt{3x-1}+(1+x)^2$"
+  ))
+  res <- .render_and_read(tmp_file, output_format = github_document(html_preview = FALSE, math_method = "webtex"))
+  expect_match(res, "^!\\[.*][(]https://latex[.]codecogs[.]com", all = FALSE)
+})
