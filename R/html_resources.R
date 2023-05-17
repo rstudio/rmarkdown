88cd47a6204b4ceda64e92694c9c6109c813c5e8
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-05-17 17:13:00 +0200

    Add tests and fix code

--- R/html_resources.R
+++ R/html_resources.R
@@ -218,12 +218,12 @@ discover_rmd_resources <- function(rmd_file, discover_single_resource) {
           'includes', 'pandoc_args', 'logo', 'reference_doc', 'reference_docx', 'template'
         )])
         # css needs to be copied for sass or bslib processing in formate rendering
-        if (
-          is_bs_theme(output_format["theme"]) ||
-          any(needed <- needs_sass(output_format['css']))
-        ) {
-          output_render_files <- c(output_render_files, unlist(output_format['css'][needed]))
+        if (is_bs_theme(resolve_theme(output_format[["theme"]]))) {
+          output_render_files <- c(output_render_files, unlist(output_format['css']))
+        } else if (any(needed <- needs_sass(output_format[['css']]))) {
+          output_render_files <- c(output_render_files, unlist(output_format[['css']][needed]))
         }
+
         lapply(output_render_files, discover_render_resource)
       }
     }
